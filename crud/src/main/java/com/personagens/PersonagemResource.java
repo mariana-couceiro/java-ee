acaa8eaf05979b6aea6111e940088e5b2bdf9a33
mariana-couceiro <8493319@formacao.iefp.pt>
2026-08-17 11:16:03 +0100

    Adicionar GET por nome e DELETE de personagem

--- crud/src/main/java/com/personagens/PersonagemResource.java
+++ crud/src/main/java/com/personagens/PersonagemResource.java
@@ -1,9 +1,11 @@
 package com.personagens;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -20,6 +22,7 @@ public class PersonagemResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response criarPersonagem(Personagem personagem) {
+
         personagens.add(personagem);
 
         return Response
@@ -32,5 +35,45 @@ public class PersonagemResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Personagem> listarPersonagens() {
         return personagens;
+    }
+
+    @GET
+    @Path("/{nome}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response procurarPersonagem(@PathParam("nome") String nome) {
+
+        for (Personagem personagem : personagens) {
+            if (personagem.getNome().equalsIgnoreCase(nome)) {
+                return Response
+                        .ok(personagem)
+                        .build();
+            }
+        }
+
+        return Response
+                .status(Response.Status.NOT_FOUND)
+                .entity("Personagem não encontrada.")
+                .build();
+    }
+
+    // DELETE - apagar pelo nome
+    @DELETE
+    @Path("/{nome}")
+    public Response apagarPersonagem(@PathParam("nome") String nome) {
+
+        boolean removida = personagens.removeIf(
+            personagem -> personagem.getNome().equalsIgnoreCase(nome)
+        );
+
+        if (removida) {
+            return Response
+                    .ok("Personagem eliminada com sucesso.")
+                    .build();
+        }
+
+        return Response
+                .status(Response.Status.NOT_FOUND)
+                .entity("Personagem não encontrada.")
+                .build();
     }
 }
